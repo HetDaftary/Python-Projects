247647420b4c2a776e1ba29d5c82d160389caab5
HetDaftary <hetdaftary@gmail.com>
2021-04-11 20:20:17 +0530

    Updating code

--- Calculator-App/src/GuiClass/MainGUI.py
+++ Calculator-App/src/GuiClass/MainGUI.py
@@ -1,57 +1,58 @@
 from PyQt5.QtWidgets import *
 from sys import exit
-from qt_material import apply_stylesheet
 from darkdetect import isDark
+from qt_material import apply_stylesheet
 
 darkTheme, lightTheme = 'dark_blue.xml', 'light_blue.xml'
+size = 600, 400
 
 class GUI:
-    buttonText = [['9', '8', '7', '+'], ['6', '5', '4', '-'], ['1', '2', '3', 'x'], ['0', 'enter', 'clear', '/']]
-    size = 600, 400
-
     def __init__(self):
-        self.app = QApplication(["Calculator"])
+        self.app = QApplication(["app"])
         self.mainWindow = QMainWindow()
-        self.mainWindow.resize(*(GUI.size))
-        
+        self.mainWindow.resize(*size)
         self.mainWin = QWidget()
-        self.mainWindow.setCentralWidget(self.mainWindow)
 
         self.initUI()
         self.initMenubar()
 
+        self.mainWindow.setCentralWidget(self.mainWin)
         self.mainWindow.show()
         exit(self.app.exec_())
 
+
     def initUI(self):
         vLayout = QVBoxLayout()
-        self.mainWin.setLayout(vLayout)
-
+        
         self.mainEntry = QLineEdit()
-        self.mainEntry.resize(200, 400)
         vLayout.addWidget(self.mainEntry)
 
-        # Time for buttons.
         self.buttonsWid = QWidget()
         gridLayout = QGridLayout()
         self.buttonsWid.setLayout(gridLayout)
 
+        self.buttonText = [['9', '8', '7', '*'], ['6', '5', '4', '+'], ['3', '2', '1', '-'], ['0', 'Ans', 'C', '/']]
         self.buttons = []
 
-        for i in range(len(GUI.buttonText)):
+        for i in range(4):
             self.buttons.append([])
-            for j in range(len(GUI.buttonText)):
-                self.buttons[i].append(QPushButton(GUI.buttonText[i][j]))
-                self.buttons[i][j].clicked.connect(lambda _, i = i, j = j : self.onPress(i, j))
+
+            for j in range(4):
+                self.buttons[i].append(QPushButton(self.buttonText[i][j]))
+                self.buttons[i][j].clicked.connect(lambda _, i = i, j = j: self.onPress(i, j))
                 gridLayout.addWidget(self.buttons[i][j], i, j)
 
         vLayout.addWidget(self.buttonsWid)
+        self.mainWin.setLayout(vLayout)
 
     def initMenubar(self):
         global darkTheme, lightTheme
+        if isDark():
+            self.setTheme(darkTheme)
+        else:
+            self.setTheme(lightTheme)
 
         self.menubar = QMenuBar()
-
         self.themeMenu = QMenu("Theme Menu")
 
         self.lightThemeAction = QAction("Light Theme")
@@ -63,21 +64,18 @@ class GUI:
         self.themeMenu.addAction(self.darkThemeAction)
 
         self.menubar.addMenu(self.themeMenu)
-
         self.mainWindow.setMenuBar(self.menubar)
 
-        if isDark():
-            self.setTheme(darkTheme)
-        else:
-            self.setTheme(lightTheme)
+    def setTheme(self, theme):
+        apply_stylesheet(self.app, theme)
 
     def onPress(self, i, j):
         if i == 3 and j == 1:
             self.mainEntry.setText(str(eval(self.mainEntry.text())))
         elif i == 3 and j == 2:
             self.mainEntry.setText("")
-        else:
-            self.mainEntry.setText(self.mainEntry.text() + GUI.buttonText[i][j])
+        else: 
+            self.mainEntry.setText(self.mainEntry.text() + self.buttonText[i][j])
 
-    def setTheme(self, theme):
-        apply_stylesheet(self.app, theme)
+if __name__ == "__main__":
+    gui = GUI()
