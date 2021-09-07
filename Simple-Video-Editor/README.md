435cac1e7c094bfe99dfbe4293efb52099c38e30
HetDaftary <hetdaftary@gmail.com>
2021-09-07 23:44:14 +0530

    Adding second feature

--- Simple-Video-Editor/README.md
+++ Simple-Video-Editor/README.md
@@ -3,3 +3,10 @@
 - A nice little Python module that uses ffmpeg to speed up simple video editing related tasks. 
 - For example, we do not need a video editor to merge videos. Ffmpeg can do this in seconds no matter how long video is.
 - Changes logged in [Changes.md](./Changes.md)
+
+## Working on features
+
+- [x] Merging videos
+    - [x] Same profile
+    - [x] To some output quality
+- [ ] Cutting Videos 
--- Simple-Video-Editor/src/MergeVideos.py
+++ Simple-Video-Editor/src/MergeVideos.py
@@ -1,17 +1,45 @@
 import os 
+from typing import *
 
 FILENAME = "mylist.txt"
 
-def mergeVideosSameProfile(videoNames, outputName):
+'''
+@breif Dependency for merge video Functions. Creates the file which stores the video names.
+This will be a temporary file which will be deleted after the merge is done.
+@param videoNames: to make the videos file
+'''
+def makeFile(videoNames: List[str]):
     global FILENAME
-    
-    with open(FILENAME, "w") as f:    
+
+    with open(FILENAME, "w") as f:
         for name in videoNames:
             f.write("file '{}'\n".format(name))
+
+'''
+@breif A function to merge same type of videos.
+@param videoNames: list of video names
+@param outputName: output name
+@return None
+'''
+def mergeVideosSameProfile(videoNames: List[str], outputName: str) -> None:
+    global FILENAME
     
+    makeFile(videoNames)    
     os.system("ffmpeg -f concat -safe 0 -i {} -c copy {}".format(FILENAME, outputName))
     os.remove(FILENAME)
 
-def mergeVideosDifferentProfile(vidoeNames, outputName, quality):
-    pass 
-    
+'''
+@breif Merge videos with rendering to a particular resolution and framerate.
+@param videoNames: list of video names
+@param outputName: output name
+@param fps: fps
+@param quality: width, height tuple.
+@return: None
+'''
+def mergeVideosDifferentProfile(videoNames: List[str], outputName: str, quality: Tuple[int], fps: int) -> None:
+    global FILENAME
+
+    makeFile(videoNames)
+
+    os.system("ffmpeg -f concat -i {} -vf scale={} -framerate {} {}".format(FILENAME, ':'.join([str(x) for x in quality]), fps, outputName))
+    os.remove(FILENAME)
